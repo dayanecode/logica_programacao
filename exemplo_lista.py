34cf41bfd647784b172f6c13a13f08072008421e
Dayane Ramos <dayanecode@gmail.com>
2023-07-07 19:00:25 -0300

    exemplo métodos que podem ser utilizados em listas

---
+++ exemplo_lista.py
@@ -0,0 +1,19 @@
+
+lista = ["DAYANE","10/04/92",31,True]
+print(lista)
+
+# append() insere um elemento no final da lista
+lista.append("Filha Amada")
+print(lista)
+
+# insert() insere um elemento em uma posição específica da lista
+lista.insert(1,"Maravilhosa")
+print(lista)
+
+# pop() remove o último item da lista
+lista.pop()
+print(lista)
+
+# clear() limpa toda a lista
+lista.clear()
+print(lista)
